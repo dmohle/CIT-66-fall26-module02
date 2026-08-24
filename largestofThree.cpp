91f2e1553bc31432b9a061b0a51e75cdad6884a8
JMcCord01 <0874017@my.scccd.edu>
2026-08-24 11:47:48 -0700

    Add files via upload

---
+++ largestofThree.cpp
@@ -0,0 +1,23 @@
+//=================================
+// File Name:    largestofThree.cpp
+// Author:       Jesse McCord
+// Date:         August 24, 2026
+// Course:       CIT-66 C++ Programming
+// Instructor:   Professor Mohle
+// Description:  Prompts for name entry, prints greeting, and pauses
+//=================================
+
+#include <iostream>
+using namespace std;
+
+int Num1=4;
+int Num2=3;
+
+int main() {
+    if(Num1>Num2) {
+        cout<<"\n Num1 > Num2 \n";
+    }
+    else {
+        cout<<"\n Num1 < Num2 \n";
+    }
+}
