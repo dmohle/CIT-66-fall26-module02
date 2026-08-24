09cb0cd421afb007eed4eeb4652d3a399b5b708e
jetstreamdan452 <danielavila45248@gmail.com>
2026-08-24 10:52:11 -0700

    Add files via upload

---
+++ helloWorldWarmup.cpp
@@ -0,0 +1,18 @@
+//=================
+// File Name:helloWorld.cpp
+// Student: Daniel Avila
+// Date: August 17, 2026
+// Course: CIT 66 C++ Programming
+// Instructor: Professor Mohle
+// Description: First C++ program displaying output conforming to style guide.
+//==================
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+    {cout << "\n\ Hello World!" <<"\n\n";
+    cout << "this is another line ";
+    return 0;
+    }
